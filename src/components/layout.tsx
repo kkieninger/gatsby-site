99bf2ca8e40ed5c72efa4a8ac7d52557c91058fe
kkieninger <ktkienin@gmail.com>
2020-12-28 11:19:47 -0500

    chore: update emotion config

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,18 +1,47 @@
-import React, { FC, ReactNode } from 'react';
+import React, { FC } from 'react';
+import { Global, css } from '@emotion/react'
+import styled from '@emotion/styled';
 import Header from './header';
 import Footer from './footer';
 
-interface Props {
-  children: ReactNode[];
-}
+/**
+ * Globals to consider:
+ * - normalize / reset
+ * - colors
+ * - layout: margin / padding
+ * - fonts
+ */
 
-const Layout: FC<Props> = ({ children }) => {
+const Wrapper = styled.div`
+  margin: 2rem auto;
+  width: 100%;
+  max-width: 900px;
+  padding: 0 1rem;
+  text-align: left;
+`;
+
+const Layout: FC = ({ children }) => {
   return (
-    <div className="global-wrapper">
+    <Wrapper>
+      <Global styles={css`
+        *,
+        :after,
+        :before {
+          box-sizing: border-box;
+        }
+
+        body {
+          background-color: #fdf9f5;
+          font-family: "Poppins", sans-serif;
+          font-weight: 500;
+          color: #313332;
+          position: relative;
+        }
+      `}/>
       <Header />
       <main>{children}</main>
       <Footer />
-    </div>
+    </Wrapper>
   );
 };
 
