a10298610ec818626581309cd13502421afa9f0c
kkieninger <ktkienin@gmail.com>
2020-12-28 18:46:32 -0500

    docs: update documentation on gatsby config

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,7 +11,10 @@ import React from 'react';
 import { jsx, ThemeProvider } from '@emotion/react';
 import { theme } from './src/styles/theme';
 
-// this wraps the entire app in our Emotion Theme
+/**
+ * This wraps the entire app in our Emotion ThemeProvider so it can be accessed
+ * throughout different pages, markdown files, components, etc.
+ */
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider theme={theme}>{element}</ThemeProvider>
 )
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,7 +3,12 @@ import React from 'react';
 import { jsx, ThemeProvider } from '@emotion/react';
 import { theme } from './src/styles/theme';
 
-// this wraps the entire app in our Emotion Theme
+/**
+ * This wrapRootElement is also needed to bundle static assets
+ * and prevent the build from failing:
+ * 
+ * https://github.com/gatsbyjs/gatsby/issues/11494
+ */
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider theme={theme}>{element}</ThemeProvider>
 )
